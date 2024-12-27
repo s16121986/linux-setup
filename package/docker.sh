c7ba22a5e40586ba18d4636325484910f88013df
Гурьянов Сергей <sergey.guryanov@online-express.ru>
2024-12-27 18:16:13 +0300

    fix docker

--- package/docker.sh
+++ package/docker.sh
@@ -3,19 +3,28 @@
 function wsi_install_docker() {
   wsi_ensure_package_installable docker "Docker already installed"
 
+  # shellcheck disable=SC2155
+  local distrib="debian"
+
   # Add Docker's official GPG key:
-  sudo apt-get update
-  sudo apt-get install ca-certificates curl gnupg
+  sudo apt-get -yqq update
+  sudo apt-get -y install ca-certificates curl
   sudo install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+  sudo curl -fsSL "https://download.docker.com/linux/$distrib/gpg" -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
 
   # Add the repository to Apt sources:
   echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$distrib \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-  sudo apt-get update
+  sudo apt-get -yqq update
 
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  if [ -z "$(getent group docker)" ]; then
+    sudo groupadd docker
+  fi
+
+  sudo usermod -aG docker "$USER"
 }
